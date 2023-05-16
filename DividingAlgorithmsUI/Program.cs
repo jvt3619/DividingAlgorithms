406de98a2b9154d0263fdec7c13c809f39a29c0e
jvt <jvt@cadblanche.eu>
2023-05-16 10:10:53 +0200

    Restructures the console program to old style

--- DividingAlgorithmsUI/Program.cs
+++ DividingAlgorithmsUI/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DividingAlgorithmsUI 
+{
+	class Program 
+    {
+    	static void Main(string[] args)
+        {
+        	System.Console.WriteLine("Hello World!");
+        }
+    }
+}
